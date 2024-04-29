fd30886aaec45b4752ca7a244e7077321fbfbaee
KotDenisV <kot.denis@gmail.com>
2024-04-29 13:41:40 +0300

    29/04 13:41

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -76,5 +76,6 @@ gallery.insertAdjacentHTML('afterbegin', markup);
 
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
+
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
